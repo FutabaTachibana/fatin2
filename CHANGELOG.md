13fc9cbef2b612fa53d78f8db1d84a9231bc77e3
Futaba_Tachibana <futabatachibana@icloud.com>
2025-12-26 21:36:52 +0800

    chore: fix some bugs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+## [0.1.1] - 2025-12-26
+
+### Fixed
+- 修复了从 Maven 仓库下载依赖时缺少 `pom.xml` 文件的问题。
+
 ## [0.1.0] - 2025-12-25
 
 ### Fixed
--- src/tachibana/src/main/java/org/f14a/tachibana/EventListener.java
+++ src/tachibana/src/main/java/org/f14a/tachibana/EventListener.java
@@ -2,53 +2,90 @@ package org.f14a.tachibana;
 
 import org.f14a.fatin2.event.command.GroupCommandEvent;
 import org.f14a.fatin2.event.command.OnCommand;
+import org.f14a.fatin2.event.session.Coroutines;
+import org.f14a.fatin2.util.MessageGenerator;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
 import java.util.Set;
 
 public class EventListener {
     private static final Set<Long> infoAllowedGroups = Set.of(893213294L);
 
+    @Coroutines
     @OnCommand(command = "info", description = "显示系统信息", usage = "info")
-    public void onInfo(GroupCommandEvent event) {
+    public void onInfo(GroupCommandEvent event) throws InterruptedException {
         if (!infoAllowedGroups.contains(event.getGroupId())) {
             return;
         }
-        OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
         StringBuilder sb = new StringBuilder();
+
+        // OS and JVM
+        SystemInfo si = new SystemInfo();
+        OperatingSystem os = si.getOperatingSystem();
+
         sb.append("操作系统: ")
-                .append(System.getProperty("os.name")).append(" ")
-                .append(System.getProperty("os.version")).append(" ")
-                .append("os.arch").append("\n");
+                .append(os).append(", ")
+                .append(os.getFamily()).append("\n");
 
         sb.append("Java: ")
-                .append(System.getProperty("java.version")).append(" ")
-                .append(System.getProperty("java.vendor")).append(" ")
+                .append(System.getProperty("java.version")).append(", ")
+                .append(System.getProperty("java.vendor")).append(", ")
                 .append(System.getProperty("java.vm.name")).append("\n");
+        sb.append("\n");
 
+        // CPU
+        HardwareAbstractionLayer hal = si.getHardware();
+        CentralProcessor cpu = hal.getProcessor();
+        int logicalCores = cpu.getLogicalProcessorCount();
+        sb.append("CPU 核心数: ").append(logicalCores).append("\n");
 
-        sb.append("CPU 核心数: ").append(os.getAvailableProcessors()).append("\n");
+        // CPU usage: need get data twice
+        long[] prevTicks = cpu.getSystemCpuLoadTicks();
+        Thread.sleep(1000);
+        double cpuUsage = cpu.getSystemCpuLoadBetweenTicks(prevTicks);
 
         sb.append("CPU 负载: ").append(String.format(
-                "%.2f%%", os.getSystemLoadAverage() * 100 / os.getAvailableProcessors()
+                "%.2f%%", cpuUsage * 100
         )).append("\n");
+
+        // Memory
+        GlobalMemory mem = hal.getMemory();
+        long totalRam = mem.getTotal();
+        long availRam = mem.getAvailable();
+        long usedRam = totalRam - availRam;
 
         sb.append("内存使用: ").append("\n");
         sb.append("Physical: ").append(String.format(
-                "%d MiB / %d MiB used.",
-                bytesToMiB(Runtime.getRuntime().freeMemory()),
-                bytesToMiB(Runtime.getRuntime().totalMemory())
-        )).append("\n");
-        sb.append("Swap: ").append(String.format(
-                "%d MiB / %d MiB used.",
-                bytesToMiB(Runtime.getRuntime().freeMemory()),
-                bytesToMiB(Runtime.getRuntime().totalMemory())
+                "%d MiB / %d MiB, %.2f%% used.",
+                bytesToMiB(usedRam),
+                bytesToMiB(totalRam),
+                ratio(usedRam, totalRam) * 100
         )).append("\n");
 
+        long totalSwap = mem.getVirtualMemory().getSwapTotal();
+        long usedSwap = mem.getVirtualMemory().getSwapUsed();
+
+        sb.append("Swap: ").append(String.format(
+                "%d MiB / %d MiB, %.2f%% used.",
+                bytesToMiB(usedSwap),
+                bytesToMiB(totalSwap),
+                ratio(usedSwap, totalSwap) * 100
+        )).append("\n");
+
+        event.send(MessageGenerator.text(sb.toString()));
     }
 
     private long bytesToMiB(long bytes) {
         return bytes / 1024 / 1024;
+    }
+    private double ratio(long used, long total) {
+        if (total <= 0) {
+            return 0.;
+        }
+        return (double) used / total;
     }
 }
